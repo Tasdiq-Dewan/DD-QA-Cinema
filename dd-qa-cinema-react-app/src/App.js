e8a02fb392db46041b322b8a9313b26c32977b65
JabKhan23 <107106989+JabKhan23@users.noreply.github.com>
2022-08-31 14:04:14 +0100

    cleaned up files - added some text

--- dd-qa-cinema-react-app/src/App.js
+++ dd-qa-cinema-react-app/src/App.js
@@ -21,7 +21,7 @@ function App() {
 ======= */}
     
     <Router>
-      <GettingThere/><Contact />
+      <GettingThere/>
     </Router>
  
 
--- dd-qa-cinema-react-app/src/Pages/GettingThere.js
+++ dd-qa-cinema-react-app/src/Pages/GettingThere.js
@@ -18,13 +18,13 @@ function GettingThere() {
     googleMapsApiKey: "AIzaSyBUodBtyw5yAUwjFxQLsaMaxYa_tIqAg-U"
   })
 
-  const [setMap] = React.useState(null)
+  const [map, setMap] = React.useState(null)
 
-//   const onLoad = React.useCallback(function callback(map) {
-//     const bounds = new window.google.maps.LatLngBounds(center);
-//     map.fitBounds(bounds);
-//     setMap(map)
-//   }, [])
+  const onLoad = React.useCallback(function callback(map) { //loads the map when page loads if this isn't here it will just be a loading page
+    // const bounds = new window.google.maps.LatLngBounds(center);
+    // map.fitBounds(bounds);
+    setMap(map)
+  }, [])
 
   const onUnmount = React.useCallback(function callback(map) {
     setMap(null)
@@ -34,15 +34,18 @@ function GettingThere() {
       <><GoogleMap
           mapContainerStyle={containerStyle}
           center={center}
-          zoom={17}
-          //onLoad={onLoad}
+          zoom={18}
+          onLoad={onLoad}
           onUnmount={onUnmount}
           MarkerF position={center}
 
       >
-          <MarkerF position={{ lat: 51.5076, lng: -0.0737 }} />
+          <MarkerF position={{ lat: 51.50755272669707, lng: -0.0737967374354214 }} />
           <></>
-      </GoogleMap><h1>Hola</h1></>
+      </GoogleMap><h1>Where we are located</h1><p>We are based right next to the Tower Bridge allowing you multiple options to travel to us. </p>
+      <p>You can travel via the underground, public transport as well as driving in. Once you have visited us, you can visit multiple tourist sites like the Tower Bridge, London Eye and Tower of London. </p>
+      <h2>QA LONDON INTERNATIONAL HOUSE CENTRE</h2><h2> 1st Floor International House 1 St Katharines Way London E1W 1UN Tel: 0345 074 7829 Fax: 0345 074 7830</h2><h3>Parking</h3><p>There's no onsite parking, however people can park at Minories Car Park</p>
+      <h3>Tube/Train</h3><p>Fenchurch Street (5 minute walk), London Bridge (15 minute walk), Liverpool Street (15 minute walk)</p></>
   ) : <></>
 }
 
