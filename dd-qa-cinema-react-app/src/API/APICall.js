2aa643f13431e79b4f7da8c9da84979fbff95575
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-09-01 10:22:02 +0100

    reorganised api files

--- dd-qa-cinema-react-app/src/API/APICall.js
+++ dd-qa-cinema-react-app/src/API/APICall.js
@@ -2,9 +2,9 @@ const connect = require("express");
 const bodyParser = require('body-parser');
 const app = connect();
 
-const FilmsAPI = require("./Films.js");
-const BookingsAPI = require("./Bookings.js");
-const ScreeningAPI = require("./Screenings.js");
+const FilmsAPI = require("./Router/FilmsAPI.js");
+const BookingsAPI = require("./Router/BookingsAPI.js");
+const ScreeningAPI = require("./Router/ScreeningsAPI.js");
 app.use("/api", bodyParser.json(), FilmsAPI, BookingsAPI, ScreeningAPI);
 const server = app.listen(8081, () =>{
     console.log("Connected");
