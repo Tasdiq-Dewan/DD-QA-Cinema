f3032b899d5cec112631c7ec00dfbd41faeb4fe8
Uche Egbon <ucheegbon904@gmail.com>
2022-09-02 12:51:37 +0100

    Created search bar and added functionality

--- dd-qa-cinema-react-app/src/App.js
+++ dd-qa-cinema-react-app/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import SearchBar from './components/Searchbar/Searchbar';
+import MockData from './MockData.json'
 function App() {
   return (
     <>
-
+    <SearchBar placeholder={'Search Term...'} data={MockData}/>
     </>
   );
 }
