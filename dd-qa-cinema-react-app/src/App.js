19758050955c4dfe86b9bed10b7b40aa909c095d
JabKhan23 <107106989+JabKhan23@users.noreply.github.com>
2022-09-02 10:27:44 +0100

    updated GettingThere and added a offers/places to visit section

--- dd-qa-cinema-react-app/src/App.js
+++ dd-qa-cinema-react-app/src/App.js
@@ -1,30 +1,8 @@
 
 
-import Contact from "./Pages/Contact";
-
-//import logo from './logo.svg';
-//import './App.css';
-import GettingThere from './Pages/GettingThere';
-import {BrowserRouter as Router} from "react-router-dom";
-
-
 function App() {
   return (
     <>
-    {/* nav
-<<<<<<< HEAD
-    search
-    router
-    
-    footer */}
-    {/* <Contact />
-======= */}
-    
-    <Router>
-      <GettingThere/>
-    </Router>
- 
-
     </>
   );
 }
