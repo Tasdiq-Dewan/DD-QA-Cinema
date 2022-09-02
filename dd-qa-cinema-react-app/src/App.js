0c9b96618cfb3f45db58a95fe823a971ed9f8b63
Uche Egbon <ucheegbon904@gmail.com>
2022-09-02 09:44:12 +0100

    Created navbar and added carousel

--- dd-qa-cinema-react-app/src/App.js
+++ dd-qa-cinema-react-app/src/App.js
@@ -1,19 +1,8 @@
-import React from 'react'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './Pages/Home';
-import Navbar from './components/Navbar/Navbar';
-import Footer from './Footer';
-
 
 
 function App() {
   return (
     <>
-    <Router>
-      <Navbar/>
-      <Home/>
-      <Footer/>
-    </Router>
     </>
   );
 }
