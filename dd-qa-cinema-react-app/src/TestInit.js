0dbf0dee706e22acf4ed63dbbb2650915a1cf65f
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-09-06 09:56:25 +0100

    changed testinit to match booking schema

--- dd-qa-cinema-react-app/src/TestInit.js
+++ dd-qa-cinema-react-app/src/TestInit.js
@@ -180,13 +180,12 @@ const BookingModel = mongoose.model("Bookings", BookingSchema);
 
 const newBooking = new BookingModel({
     CustomerRef: "90AB223",
+    CustomerName: "Andrew McCall",
     Seats: ["15", "16"],
-    Tickets : {
-        AdultPrice: 7,
-        ChildPrice: 5,
-        AdultQuantity: 1,
-        ChildQuantity: 1
-    },
+    AdultPrice: 7,
+    ChildPrice: 5,
+    AdultQuantity: 1,
+    ChildQuantity: 1,
     AmountPaid: 12,
     TransactionRef: 1239875987,
     Screening: {
