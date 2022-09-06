8d75f70182f88deba0c0e23918b7f988846548db
JabKhan23 <107106989+JabKhan23@users.noreply.github.com>
2022-09-06 11:33:04 +0100

    Discussion styled up and finished

--- dd-qa-cinema-react-app/src/Axios/Discussions/DiscussionsAxios.js
+++ dd-qa-cinema-react-app/src/Axios/Discussions/DiscussionsAxios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import "./DiscussionAxios.css";
+import Filter from 'bad-words';
 
 const PostDiscussion = () =>{
     const [userPost, setPost] = useState([]);
@@ -7,6 +9,8 @@ const PostDiscussion = () =>{
     const [Title, setTitle] = useState("");
     const [Message, setMessage] = useState("");
     const [Posts, getPosts] = useState([]);
+    var Filter = require('bad-words'),
+    filter = new Filter();
 
     const getUsername = (e) => {
         setUser(e.target.value);
@@ -21,6 +25,10 @@ const PostDiscussion = () =>{
     }
 
     const addPost = (e) => {
+        if (Title.length ===0 || Message ===0){
+            alert("Please complete the required fields");
+        }
+        else{
         axios.post("http://localhost:8081/api/addDiscussionPost", {
         "Post_id":97,
         "Username":User,
@@ -31,6 +39,8 @@ const PostDiscussion = () =>{
             console.log(result.data);
             window.location.reload();
         })
+  
+    }
     }
 
     useEffect(() => {
@@ -40,23 +50,26 @@ const PostDiscussion = () =>{
     }, []);
 
     return (
-       <>
-
-           <input type="text" className = 'search-input' placeholder= "Username" onChange={e => getUsername(e)}></input>
-           <input type="text" className = 'search-input' placeholder= "Title" onChange={e => getTitle(e)}></input>
-           <input type="text" className = 'search-input' placeholder= "Message" onChange={e => getMessage(e)}></input>
-
-           <button onClick={e => addPost(e)} >tutti</button>
+       <div className = 'background'>
+        <h3 className ='text'>Please enter a username, you can post anonymously if you desire</h3>
+           <input type="text" className = 'username-input' placeholder= "Username" onChange={e => getUsername(e)}></input>
+           <h3 className ='text'>Enter the title of what you would like to discuss</h3>
+           <input type="text" className = 'title-input' placeholder= "Title" onChange={e => getTitle(e)}></input>
+           <h3 className ='text'>Type in your message here, please be civil and any bad language will be censored</h3>
+           <input type="text" className = 'data-input' placeholder= "Message" onChange={e => getMessage(e)}></input>
+            
+           <button className = 'button-56' onClick={e => addPost(e)} >Create Topic</button>
 
            {Posts.map(post => (
-                <div>
-            <h3>Username: {post.Username}</h3>
-            <h4>Title: {post.PostTitle}</h4>
-            <p>Message: {post.PostMessage}</p>
+                <div className='discussion-Table'>
+            <h2 className='title-Text'>Title: {filter.clean(post.PostTitle)}</h2>
+            <h3 className='user-Text'>Username: {filter.clean(post.Username)}</h3>
+            <p className='message-Text'>Message: {filter.clean(post.PostMessage)}</p>
             </div>
            ))}
 
-           </>
+                
+           </div>
     );
 
 }
